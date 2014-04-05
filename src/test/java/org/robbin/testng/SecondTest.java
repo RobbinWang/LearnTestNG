170aae95b53ea8c6185590f52c6a5e2b3ff63d09
robbin <mianbao5481@126.com>
2014-04-05 18:59:51 +0800

    testng.xml class

---
+++ src/test/java/org/robbin/testng/SecondTest.java
@@ -0,0 +1,17 @@
+package org.robbin.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * User: robbin
+ * Date: 4/5/14
+ * Time: 6:25 PM
+ */
+public class SecondTest {
+
+    @Test
+    public void test()
+    {
+            System.out.println("this is my second Test class");
+    }
+}
